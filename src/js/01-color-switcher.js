5a41b711757d66a7512084a66182ca51fb6f0828
LukaszBomba <101752120+LukaszBomba@users.noreply.github.com>
2022-08-16 22:18:28 +0200

    .
    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,22 +5,16 @@ const body = document.querySelector('body');
 let timer = null;
 
 function getRandomHexColor() {
-  const randomColor= `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   return randomColor;
-};
+}
 
-
-btnStart.addEventListener('click',() => {
-   btnStart.disabled = true;
-   btnStop.disabled = false;
-   timer = setInterval(()=>{
-      body.style.backgroundColor = `${getRandomHexColor()}`;
-   },1000)
+btnStart.addEventListener('click', () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
 });
 
 btnStop.addEventListener('click', () => {
-   btnStart.disabled = false;
-   btnStop.disabled = true;
-   clearInterval(timer);
- });
- 
+  clearInterval(timer);
+});
