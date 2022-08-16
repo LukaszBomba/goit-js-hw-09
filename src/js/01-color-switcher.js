86259878a573d869b7b87def69cf9b6cd3a5fc65
LukaszBomba <101752120+LukaszBomba@users.noreply.github.com>
2022-08-16 11:32:30 +0200

    Update task
    Task first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+
+const start = document.querySelector('[data-start]');
+
+const stop = document.querySelector('[data-stop]');
+
+let color = null;
+
+start.addEventListener('click', () => {
+  color = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.background = color;
+  }, 1000);
+});
+
+stop.addEventListener('click', () => {
+  clearInterval(color);
+});
